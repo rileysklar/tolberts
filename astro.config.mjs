c4983236f0133952d3b86ed83ab7fd7563e7af9c
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2024-01-01 22:39:07 -0600

    sitemap implement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+  site: "https://andrewriefenstahlcom",
+  integrations: [tailwind(), sitemap()],
 });
