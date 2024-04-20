475ae24cbe030cd1f3157b711d0ac5b71d5f8da8
Riley Sklar <riley.sklar@gmail.com>
2024-04-20 13:20:48 -0500

    topnav

---
+++ src/components/ui/TopNav.jsx
@@ -0,0 +1,30 @@
+import { Button } from "./button";
+
+export default function TopNav() {
+  return (
+    <nav
+      className="flex w-full justify-between border-b bg-black
+       p-4 text-xl font-semibold text-white"
+    >
+      <div className="mx-auto flex w-full max-w-7xl ">
+        <div className="logo w-[100px]">
+          <img src="src/assets/logo-white.png" alt="Logo" />
+        </div>
+        <div className="menu flex w-full justify-end space-x-4">
+          <Button variant="link" className="text-2xl text-white">
+            Home
+          </Button>
+          <Button variant="link" className="text-2xl text-white">
+            Menu
+          </Button>
+          <Button variant="link" className="text-2xl text-white">
+            About
+          </Button>
+          <Button variant="link" className=" text-2xl text-white">
+            Order
+          </Button>
+        </div>
+      </div>
+    </nav>
+  );
+}
---
+++ src/lib/wordpress/fetchEvents.ts
@@ -0,0 +1,58 @@
+import fetchAPI from "./api";
+
+export async function getEvents() {
+  const data = await fetchAPI(
+    `query AllEvents {
+        events(first: 100) {
+            edges {
+              node {
+                id
+                postTypeEvent {
+                  date
+                  primaryHeader
+                  secondaryHeader
+                  image {
+                    sourceUrl
+                  }
+                  description
+                  endTime
+                  startTime
+                }
+              }
+            }
+        }
+    }
+`,
+    {
+      variables: {},
+    },
+  );
+  return data?.events?.edges;
+}
+
+export async function getSingleEvent(id) {
+  const data = await fetchAPI(
+    `
+      query getSingleEvent($id:ID!){
+          event(id:$id){   
+            id,        
+            postTypeEvent {
+              cta
+              date
+              location
+              name
+              primaryHeader
+              secondaryHeader
+              image {
+                  sourceUrl
+              }
+              description
+            }
+          }
+        }
+      `,
+    { id },
+  );
+
+  return data?.event;
+}
