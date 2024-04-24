8a4eb46dcf882358d50ec7e2bbce1e485c46f1fb
Riley Sklar <rileysklar@rileys-mbp-2.lan>
2024-04-24 12:19:28 -0500

    mobile nav and animated menu

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,13 @@ module.exports = {
       screens: {
         "2xl": "1400px",
       },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
     },
     extend: {
       colors: {
