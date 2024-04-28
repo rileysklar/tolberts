5959f35f0a41467bedc47edeb3f9a90c78fad0ec
Riley Sklar <riley.sklar@gmail.com>
2024-04-27 19:50:28 -0700

    image trouble2

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -3,22 +3,22 @@ function PhotoGallery() {
     <div>
       <div className="grid grid-cols-2 sm:grid-cols-4">
         <img
-          src="/public/tlb-1.jpg"
+          src="/tlb-1.jpg"
           alt="Description of the image"
           className="h-auto w-full object-cover object-center"
         />
         <img
-          src="../../public/tlb-2.jpg"
+          src="./tlb-2.jpg"
           alt="Description of the image"
           className="h-full w-full object-cover object-center"
         />
         <img
-          src="../../public/tlb-4.jpg"
+          src="/tlb-4.jpg"
           alt="Description of the image"
           className="h-full w-full object-cover object-center"
         />
         <img
-          src="../../public/tlb-4.jpg"
+          src="/tlb-4.jpg"
           alt="Description of the image"
           className="h-full w-full object-cover object-center"
         />
